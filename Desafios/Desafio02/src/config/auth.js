6c683c61ac79506787f2049be3b292adc16a1ffa
guilhermecortiz <guilhermecortiz@gmail.com>
2019-11-01 12:55:00 -0300

    Desafio 02.03

---
+++ Desafios/Desafio02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6a39044d8a57f12dbd4eaab0e504d5e5',
+  expiresIn: '7d',
+};
--- Desafios/Desafio02/src/routes.js
+++ Desafios/Desafio02/src/routes.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  return res.json({ message: `Hello World 3` });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme Ortiz',
+    email: 'guilherme@rocketseat.com.br',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
